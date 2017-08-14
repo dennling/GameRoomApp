7921f50beb0da73f4c117e63c95a007ec3ce742b
Dennis Ling <dennis.ling@duke.edu>
2017-08-14 16:23:48 -0700

    add more functionality to gameplay

--- src/model/gameplay/Player.java
+++ src/model/gameplay/Player.java
@@ -2,8 +2,6 @@ package model.gameplay;
 
 import model.Card;
 
-import java.util.ArrayList;
-
 /**
  * Created by lingd on 8/9/17.
  */
@@ -12,12 +10,15 @@ public class Player {
     private int chips;
     private Card[] hand;
     private Game g;
+    private int currBetAmount;
+    private boolean bb;
 
     public Player(String name, int chips, Game g){
         this.name = name;
         this.chips = chips;
         hand = new Card[2];
         this.g = g;
+        this.currBetAmount = 0;
     }
 
     public void setHand(Card one, Card two){
@@ -33,11 +34,26 @@ public class Player {
         this.chips = chips;
     }
 
-    public void bet(int amt, int pos){
-        g.bet(this,amt,pos);
+    public Card[] getHand() { return hand; }
+
+    public void bet(int amt, int whichPot){
+        int diff = amt-currBetAmount;
+        this.currBetAmount = amt;
+        g.bet(this, diff, whichPot);
     }
+
+    public String getName() {return name;};
 
     public void fold(){
         g.fold(this);
+        this.currBetAmount = 0;
+    }
+
+    public void setCurrBetAmount(int set){
+        this.currBetAmount = set;
+    }
+
+    public int getCurrBetAmount(){
+        return this.currBetAmount;
     }
 }
